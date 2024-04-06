9fca726d48b977bba626e4d378b75f2ba1c82db9
NahueSalinas <nsalinasolivera@lasalleflorida.edu.ar>
2024-04-06 12:24:14 -0300

    Se agregaron nuevos comandos y especificaziones

--- index.js
+++ index.js
@@ -23,26 +23,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 /****************************************
  * Business
 ****************************************/
-
-app.get('/code', async (req, res) => {
-  try{
-    
-    }catch(e){
-      res.status(200).send({'Codigos de Programación Basicos': 'Se utilizan normalmente Html, Js(JavaScript), CSS, C#, C+, C++ y C'})
-    }
+app.post('/data', (req, res) => {
+  const datosRecibidos = req.body;
+  datos = datosRecibidos;
+  res.json({ 'msg': 'Datos Recibidos Correctamente'});
 })
 
-app.get('/How', async (req, res) => {
-  res.status(200).json({ message: "Como aprendemos a programar?, bueno en caso que no puedas permitirte mirar video en YT, podes usar la normal herramienta IA ChatGpt que te puede ayudar en todos los lenguajes de programación o tambien la IA de programación BlackBox que esta solo especializada en los lenguajes de programación en incluso sabe mas que chat gpt en terminos de la programación." })
+app.put('/data/update', (req, res) => {
+  const datosActualizados = req.body;
+  datos = datosActualizados;
+  res.json({ 'msg': 'Datos Actualizados Correctamente'})
 })
-
-app.get('/Codigos', async (req, res) => {
-  res.status(403).send({
-    'error': 'Perdón pero no te puedo dar acceso a los codigos de la pagina'
-  })
-})
-
-
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
